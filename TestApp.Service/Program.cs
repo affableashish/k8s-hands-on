8c47827cb6fae7e2f69db362fd65683586ce6bef
Ashish Khanal <affableashk@outlook.com>
2023-11-15 22:48:01 -0500

    A working deployment!

--- TestApp.Service/Program.cs
+++ TestApp.Service/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// The liveness check
+app.MapHealthChecks("/healthz/live", new HealthCheckOptions
+{
+    // This liveness check filters out all health checks by returning false,
+    // so no health checks would run for this endpoint
+    Predicate = _ => false
+});
 
 app.Run();
