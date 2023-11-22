51464575c712789f3d52d39406e7609d02e314d7
Ashish Khanal <affableashk@outlook.com>
2023-11-22 17:27:13 -0500

    Init containers working 🎉

--- TestApp.Cli/Program.cs
+++ TestApp.Cli/Program.cs
@@ -7,7 +7,6 @@ if (args.Length > 0 && args[0] == "say-hello")
 else if (args.Length > 0 && args[0] == "migrate-database")
 {
     Console.WriteLine("Running migrations...");
-    //Thread.Sleep(30_000);
     await Task.Delay(30_000);//  Thread.Sleep(30_000);
     Console.WriteLine("Migrations complete!");
 }
